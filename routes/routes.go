3cb993a292d4dbcfda5904127790e9200c8e06bc
Rahmat Gunawan <rahmat@klinikpintar.id>
2025-01-25 09:51:57 +0700

    add checker

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 
 // RegisterRoutes mendaftarkan semua rute API
 func RegisterRoutes(r *gin.Engine) {
+	r.GET("/check-role", handlers.CheckRole) // Tambahkan rute check-role
 	r.GET("/people", handlers.GetPeople)
 	r.GET("/people/:id", handlers.GetPersonByID)
 	r.POST("/people", handlers.CreatePerson)
